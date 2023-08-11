bf308b8e21b9cd3b663b220a1cc69eacb7d22f3c
sukhi10 <sshergill0102@gmail.com>
2023-08-10 23:11:44 -0400

    Lastpage almost done

---
+++ lastpage/lastpage.js
@@ -0,0 +1,10 @@
+function closewindow() {
+    let cww = confirm("Are you sure?"); 
+    if(cww== true) { 
+        window.close(); 
+    }
+    else{ 
+        alert("Great answer!"); 
+    }
+  }
+  
